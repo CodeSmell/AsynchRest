28e029e7b4e1e2c458c750a79aeefa757e46581a
CodeSmell <mbarlotta@gmail.com>
2016-11-22 17:32:51 -0500

    Spring configuration

--- src/main/java/codesmell/config/Config.java
+++ src/main/java/codesmell/config/Config.java
@@ -2,11 +2,24 @@ package codesmell.config;
 
 import codesmell.foo.FooBar;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@ComponentScan(basePackages = { "codesmell.config" })
+@PropertySource(value = { "classpath:config.props" })
 public class Config {
 
+    /**
+     * used by Spring to resolve @Value configuration
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfig() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+    
     @Bean
     public FooBar doFooBar() {
         return new FooBar();
--- src/main/java/codesmell/foo/FooBar.java
+++ src/main/java/codesmell/foo/FooBar.java
@@ -2,11 +2,21 @@ package codesmell.foo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 
-public class FooBar {
+public class FooBar implements InitializingBean {
     private static final Logger LOGGER = LoggerFactory.getLogger(FooBar.class);
+
+    @Value("${hello}")
+    private String helloValue;
     
     public FooBar() {
         LOGGER.debug("foo bar!");
+    }
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        LOGGER.debug("the value of the property hello is:" + helloValue);
     }
 }
