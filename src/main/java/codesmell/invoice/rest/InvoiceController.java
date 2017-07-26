483e85fee77b59b90f45077fa3bd0b984c01797c
CodeSmell <mbarlotta@gmail.com>
2017-07-26 15:16:45 -0400

    SpringBoot mockMvc test for simple controller

--- src/main/java/codesmell/invoice/rest/InvoiceController.java
+++ src/main/java/codesmell/invoice/rest/InvoiceController.java
@@ -4,10 +4,17 @@ import codesmell.foo.FooBar;
 import codesmell.invoice.dao.InvoiceActorType;
 import codesmell.invoice.dao.InvoiceMetaData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,15 +23,23 @@ public class InvoiceController {
     @Autowired
     FooBar foobar;
 
-    @RequestMapping("/foo")
+    @RequestMapping(
+            path = "/foo",
+            method = RequestMethod.GET)
+    @ResponseBody
     public String fooBar() {
         return "Hello " + foobar.getHello();
     }
 
-    @RequestMapping("/find")
-    public InvoiceMetaData findInvoice(
+    @RequestMapping(
+            path = "/invoice/find",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<InvoiceMetaData>> findInvoice(
             @RequestParam(value = "storeNumber") String storeNumber,
             @RequestParam(value = "trailerNumber") String trailerNumber) {
+
+        List<InvoiceMetaData> list = new ArrayList<>();
 
         InvoiceMetaData meta = InvoiceMetaData.builder()
                 .withInternalId(UUID.randomUUID().toString())
@@ -40,7 +55,9 @@ public class InvoiceController {
                 .end()
                 .build();
 
-        return meta;
+        list.add(meta);
+
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
 }
---
+++ src/test/java/codesmell/invoice/rest/InvoiceControllerMockMvcTest.java
@@ -0,0 +1,50 @@
+package codesmell.invoice.rest;
+
+import codesmell.invoice.config.Config;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = InvoiceController.class)
+@EnableWebMvc
+@AutoConfigureMockMvc()
+@ContextConfiguration(classes = Config.class)
+public class InvoiceControllerMockMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void test_foobar_hello() throws Exception {
+        this.mockMvc.perform(get("/foo"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello world"));
+    }
+
+    @Test
+    public void test_invoice() throws Exception {
+        this.mockMvc.perform(get("/invoice/find")
+                .accept(MediaType.APPLICATION_JSON)
+                .param("storeNumber", "100")
+                .param("trailerNumber", "YT-1300"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(jsonPath("$.length()", Matchers.is(1)))
+            .andExpect(jsonPath("$[0].internalId", Matchers.notNullValue()))
+            .andExpect(jsonPath("$[0].invoiceDestination.actorName", Matchers.is("100")))
+            .andExpect(jsonPath("$[0].trailerNumber", Matchers.is("YT-1300")));
+    }
+
+}
